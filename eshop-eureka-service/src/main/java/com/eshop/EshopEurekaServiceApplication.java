40c593570c3ee760944777dc4937d4718ea4fbe9
SuperZiyear <SuperZiyear@163.com>
2019-07-14 16:18:36 +0800

    Eureka服务创建完成

--- eshop-eureka-service/src/main/java/com/eshop/EshopEurekaServiceApplication.java
+++ eshop-eureka-service/src/main/java/com/eshop/EshopEurekaServiceApplication.java
@@ -1,13 +1,18 @@
 package com.eshop;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EshopEurekaServiceApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EshopEurekaServiceApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(EshopEurekaServiceApplication.class, args);
+        LOGGER.info("============== EurekaService启动成功 ===================");
     }
-
 }
